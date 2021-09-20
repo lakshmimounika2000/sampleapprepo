26978f84d4ef6882941f151a6e2cde718451ac18
lakshmimounika2000 <mounika.bl000@gmail.com>
2021-09-20 11:03:08 +0530

    show method added

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -5,7 +5,12 @@ public class BootClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello!");
+		show();
 
+	}
+	
+	public static void show() {
+		System.out.println("show message here!!");
 	}
 
 }
